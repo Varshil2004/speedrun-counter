5c3e661a550015d98a460b9031167f3dc943c93b
KUKADIYA VARSHIL <152945612+Varshil2004@users.noreply.github.com>
2025-10-25 18:03:12 +0530

    Update .lintstagedrc.js

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -6,7 +6,7 @@ const buildNextEslintCommand = (filenames) =>
     .join(" --file ")}`;
 
 const checkTypesNextCommand = () => "yarn next:check-types";
-
+//this js file for all functions which can help in this counter.
 const buildHardhatEslintCommand = (filenames) =>
   `yarn hardhat:lint-staged --fix ${filenames
     .map((f) => path.relative(path.join("packages", "hardhat"), f))
