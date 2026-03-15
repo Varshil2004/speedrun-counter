74a8d4032257ddcecea0193c775dcd806d5e0664
KUKADIYA VARSHIL <152945612+Varshil2004@users.noreply.github.com>
2026-03-15 11:52:34 +0530

    Update .lintstagedrc.js

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,4 @@
 //this is a speeddrun - counter code.
-//this is a java script code.
 const path = require("path");
 const buildNextEslintCommand = (filenames) =>
   `yarn next:lint --fix --file ${filenames
